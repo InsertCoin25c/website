611e71c1436709f7cd8d4778c18df184d37c1c7a
InsertCoin Team <insertcoin.s25c@gmail.com>
2021-10-12 12:19:10 +0200

    Update Minting App

---
+++ build/app.js
@@ -0,0 +1,143 @@
+
+// const cabin = document.getElementById("cabinato");
+// const insertButton = document.getElementById("insertButton");
+// const realSite = document.getElementById("realSite");
+const discordButtons = document.getElementsByClassName("discordButton");
+const telegramButtons = document.getElementsByClassName("telegramButton");
+const twitterButtons = document.getElementsByClassName("twitterButton");
+const instagramButtons = document.getElementsByClassName("instagramButton");
+// const discordButtonsBig = document.getElementsByClassName("discordButtonBig");
+// const telegramButtonsBig = document.getElementsByClassName("telegramButtonBig");
+// const twitterButtonsBig = document.getElementsByClassName("twitterButtonBig");
+// const copyButtons = document.getElementsByClassName("copyButton");
+// const homeButton = document.getElementById("homeButton");
+// const homeButtons = document.getElementsByClassName("homeButton");
+const chartButtons = document.getElementsByClassName("openChartButton");
+const getCoinButtons = document.getElementsByClassName("openGetCoinButton");
+const getNFTButtons = document.getElementsByClassName("openGetNFT");
+const etherescanButtons = document.getElementsByClassName("openEtherScan");
+const interactFunctions = {
+    // zoomCabin: function () {
+    //     cabin.classList.add("arcade-machine-zoom");
+    //     insertButton.classList.add("insertButton-zoom");
+    //     setTimeout(function(){
+    //         cabin.style.display = "none";
+    //         realSite.style.display = "unset";
+    //         realSite.classList.add("real-site-zoom");
+    //         cabin.classList.remove("arcade-machine-zoom");
+    //         insertButton.classList.remove("insertButton-zoom");
+    //     }, 1000);
+
+    // },
+    // homeButton: function () {
+    //     realSite.classList.add("dissolve-real-site");
+    //     setTimeout(function(){
+    //         realSite.classList.remove("real-site-zoom");
+    //         realSite.style.display = "none";
+    //         realSite.classList.remove("dissolve-real-site");
+    //         cabin.style.opacity = "0";
+    //         cabin.style.display = "unset";
+    //         cabin.classList.add("return-cabin");
+    //         setTimeout(function(){
+    //             cabin.style.opacity = "1";
+    //             cabin.classList.remove("return-cabin");
+    //         }, 1000);
+    //     }, 250);
+
+    // },
+    discordButton: function () {
+        window.open("https://discord.gg/nUSSPNMfZ2");
+    },
+    telegramButton: function () {
+        window.open("https://t.me/InsertCoin25c");
+    },
+    twitterButton: function () {
+        window.open("https://twitter.com/InsertCoin25C");
+    },
+    instagramButton: function () {
+        window.open("https://www.instagram.com/insertcoin25c_official/");
+    },
+    copyButton: function () {
+        alert("Non mangiare dove si copia!");
+    },
+    chartButton: function () {
+        // window.open("https://www.dextools.io/app/uniswap/pair-explorer/0x465d676bc576eac77f3a69c5454a84294cea5092");
+        alert("Coming soon!");
+    },
+    getCoinButton: function () {
+        // window.open("https://app.uniswap.org/#/swap?outputCurrency=0x76851a93977bea9264c32255b6457882035c7501&use=V2");
+        alert("Coming soon!");
+    },
+    getNFTButton: function () {
+        // window.open("https://opensea.io/");
+    },
+    etherescanButton: function () {
+        // window.open("https://etherscan.io/address/0x08b088e282cDF720796809514F5E07096c5A8c6f");
+        alert("Coming soon!");
+    }
+}
+// insertButton.addEventListener("mousedown", interactFunctions.zoomCabin);
+// homeButton.addEventListener("mousedown", interactFunctions.homeButton);
+// copyButton.addEventListener("mousedown", interactFunctions.copyButton);
+// for (const button in copyButtons) {
+//     copyButtons.item(button).addEventListener("mousedown", interactFunctions.copyButton);
+// }
+// for (const button in homeButtons) {
+//     homeButtons.item(button).addEventListener("mousedown", interactFunctions.homeButton);
+// }
+for (const button in discordButtons) {
+    discordButtons.item(button).addEventListener("mousedown", interactFunctions.discordButton);
+}
+for (const button in telegramButtons) {
+    telegramButtons.item(button).addEventListener("mousedown", interactFunctions.telegramButton);
+}
+for (const button in twitterButtons) {
+    twitterButtons.item(button).addEventListener("mousedown", interactFunctions.twitterButton);
+}
+for (const button in instagramButtons) {
+    instagramButtons.item(button).addEventListener("mousedown", interactFunctions.instagramButton);
+}
+
+for (const button in chartButtons) {
+    chartButtons.item(button).addEventListener("mousedown", interactFunctions.chartButton);
+}
+
+for (const button in getCoinButtons) {
+    getCoinButtons.item(button).addEventListener("mousedown", interactFunctions.getCoinButton);
+}
+
+// for (const button in getNFTButtons) {
+//     getNFTButtons.item(button).addEventListener("mousedown", interactFunctions.getNFTButton);
+// }
+
+for (const button in etherescanButtons) {
+    etherescanButtons.item(button).addEventListener("mousedown", interactFunctions.etherescanButton);
+}
+
+const clipboard = new ClipboardJS('.copyButton', {
+    text: function () {
+        return "0xcB00ed46D3e84f144d067eaaFf36c3C064138EC3";
+    }
+});
+
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
+
+// for (const button in discordButtonsBig) {
+//     discordButtonsBig.item(button).addEventListener("mousedown", interactFunctions.discordButton);
+// }
+// for (const button in telegramButtonsBig) {
+//     telegramButtonsBig.item(button).addEventListener("mousedown", interactFunctions.telegramButton);
+// }
+// for (const button in twitterButtonsBig) {
+//     twitterButtonsBig.item(button).addEventListener("mousedown", interactFunctions.twitterButton);
+// }
